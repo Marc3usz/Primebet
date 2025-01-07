dd13da3ab3ee2549e0e3018f0d502dad085ff3ec
Maksiosmf <136759557+maksiosmf@users.noreply.github.com>
2025-01-07 15:23:57 +0100

    lol

--- primebet/src/constants/links.ts
+++ primebet/src/constants/links.ts
@@ -1,3 +1,9 @@
 export const Links = {
-    
+    HOMEPAGE: "/",
+    LOGIN: "/login",
+    REGISTER: "/login?modal=register",
+    FORGOTPASS: "/login?modal=forgotpass",
+    SETTINGS: "/settings",
+    BETS: "/bets:settled",
+    OFFERS: "/offers:filter",
 }

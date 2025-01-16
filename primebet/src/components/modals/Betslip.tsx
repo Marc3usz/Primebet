fa3b987ebad8be8872c731112a125b4a9513e677
Marcel Geba <u35_margeb_lbn@technischools.com>
2025-01-16 14:49:43 +0100

    working on homepage

--- primebet/src/components/modals/Betslip.tsx
+++ primebet/src/components/modals/Betslip.tsx
@@ -1,20 +1,27 @@
 export const Betslip = ({
     isVisible,
     onClose,
+    clearBetslip,
 }: {
     isVisible: boolean;
     onClose: () => void;
+    clearBetslip: () => void;
 }) => {
     return (
         <div className="fixed bottom-4 right-4 bg-slate-800 shadow-md rounded-md p-4 w-64 h-fit transition-all">
-            <div className="flex justify-between items-center mb-2">
+            <div className="flex justify-between items-center mb-2 flex-row">
                 <h3 className="text-lg font-semibold text-white">Betslip</h3>
+                <div className="w-fit flex gap-3">
+                {
+                    isVisible || <button onClick={clearBetslip} className="text-white hover:text-slate-300">X</button>
+                }
                 <button
                     onClick={onClose}
                     className="text-white hover:text-slate-300"
                 >
                     {isVisible ? "⮝" : "⮟"}
                 </button>
+                </div>
             </div>
             <div className={(isVisible ? "h-0" : "h-[70vh]") + " transition-all overflow-hidden flex"}>
             <p className="text-sm text-white">
--- primebet/src/pages/Homepage.tsx
+++ primebet/src/pages/Homepage.tsx
@@ -9,7 +9,7 @@ import { Betslip } from "../components/modals/Betslip";
 const PLACEHOLDER = () => {
     const liveUserData = useStore(userData);
     return <div className="w-fit h-fit">
-    <h1>ADD EXAMPLE BET</h1>
+    <h1 className="text-base">ADD EXAMPLE BET</h1>
     <button onClick={() => liveUserData.appendBetslip({title: "FC Barcelona vs Real Madrid", datetime: (new Date).getUTCDate(), odds: 1})}>Add Test Bet</button>    
     </div>
 }
@@ -31,7 +31,7 @@ export const Homepage = () => {
                 <PLACEHOLDER />
                 <Outlet />
             </div>
-            {!!!liveUserData.betslip || <Betslip isVisible={isModalVisible} onClose={toggleModal} />}
+            {!!!liveUserData.betslip || <Betslip isVisible={isModalVisible} onClose={toggleModal} clearBetslip={liveUserData.emptyBetslip}/>}
         </div>
     );
 };

a2f6a1914096af12cdd0094487ddcf07df67cff5
Marcel Geba <u35_margeb_lbn@technischools.com>
2025-01-17 18:39:13 +0100

    doing bets displaying

--- primebet/src/components/Offers.tsx
+++ primebet/src/components/Offers.tsx
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useParams, } from "react-router-dom";
 import { fetchMap } from "../constants/fetch";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Offers = () => {
     const { filter } = useParams();
+    const [queryResults, setQueryResults] = useState({});
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
     useEffect(() => {
-        fetchMap[filter + ""]?.()?.then((res) => console.log(res));
+        fetchMap[filter + ""]?.()?.then((res) => setQueryResults(res)).catch(() => setErrorMessage("Error: Bets couldn't load"));
     }, []);
-    return <h1>filter: {filter}</h1>;
+
+
+    return <h1>{errorMessage ?? filter}</h1>;
 };
